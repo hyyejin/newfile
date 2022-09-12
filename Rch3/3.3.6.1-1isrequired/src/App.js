2bb12e20124cfb50aee8c38a6d9bcaec5063f26a
unknown <reall0108@naver.com>
2022-09-12 12:08:52 +0900

    3.3.6-1

---
+++ Rch3/3.3.6.1-1isrequired/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MyComponent from './mycomponent';
+ 
+const App = () => {
+  return <MyComponent name="React">리액트</MyComponent>;
+};
+ 
+export default App;
+
+
+
