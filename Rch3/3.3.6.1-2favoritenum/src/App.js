1f8a371822228138c4a2716d4f051551064e4664
unknown <reall0108@naver.com>
2022-09-12 12:17:08 +0900

    3.3.6.1-2

---
+++ Rch3/3.3.6.1-2favoritenum/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MyComponent from './mycomponent';
+
+
+const App = () => {
+  return (
+    <MyComponent name="React" favoriteNumber={1}>
+      리액트
+    </MyComponent>
+  );
+};
+
+
+
+export default App;
