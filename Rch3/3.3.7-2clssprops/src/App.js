4d63a92a3f3f395de9ebaaf7aa18bef7a8e6a488
unknown <reall0108@naver.com>
2022-09-12 12:49:19 +0900

    3.3.7-2

---
+++ Rch3/3.3.7-2clssprops/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import MyComponent from './mycomponent';
+
+
+const App = () => {
+  return (
+    <MyComponent name="React" favoriteNumber={1}>
+      리액트
+    </MyComponent>
+  );
+};
+
+
+
+export default App;
+
+
