d108f2949ad32f04ae9fb56b8ef7986bc48dba24
unknown <reall0108@naver.com>
2022-09-15 12:23:53 +0900

    8.2.3-1

--- Rch8/8.2.3cleanup/src/Info.js
+++ Rch8/8.2.3cleanup/src/Info.js
@@ -4,11 +4,14 @@ import React, { useState, useEffect } from 'react';
 const Info = () => {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
-
   useEffect(() => {
-      console.log('마운트될 때만 실행됩니다.');
-    }, []);
-  
+    console.log('effect');
+    console.log(name);
+    return () => {
+      console.log('cleanup');
+      console.log(name);
+    };
+  });
  
   const onChangeName = e => {
     setName(e.target.value);
@@ -40,7 +43,7 @@ const onChangeNickname = e => {
   );
 };
 
+
+
+
 export default Info;
-
-
-
