ca3b67a0d4885236b5d49c721b6a64addbecbd3e
unknown <reall0108@naver.com>
2022-09-16 09:07:14 +0900

    8.3.2

---
+++ Rch8/8.3.2name/src/Info.js
@@ -0,0 +1,53 @@
+import React, { useReducer } from 'react';
+
+
+function reducer(state, action) {
+    // action.type에 따라 다른 작업 수행
+    switch (action.type) {
+      case 'INCREMENT':
+        return { value: state.value + 1 };
+      case 'DECREMENT':
+        return { value: state.value - 1 };
+      default:
+        // 아무것도 해당되지 않을 때 기존 상태 반환
+   
+        return state;
+        [action.name]: action.value  
+    }
+  }
+
+
+const Info = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: ""
+  });
+  const { name, nickname } = state;
+  const onChange = e => {
+    dispatch(e.target);
+  };
+
+
+
+return (
+    <div>
+      <div>
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임: </b>
+          {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+export default Info;
