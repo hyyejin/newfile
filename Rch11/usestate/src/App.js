1698730d814eca5e1f86fac7ade58ac13f7f1399
hyejin <reall0108@naver.com>
2022-09-23 09:40:09 +0900

    11.5.1

---
+++ Rch11/usestate/src/App.js
@@ -0,0 +1,71 @@
+import React, { useRef, useState, useCallback } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
+
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: <div><span class="co31">할</span> <span class="co31">일</span> <span class="co49">$</span><span class="co31"></span>
+            </div>,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+
+
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos);
+
+
+
+// 고윳값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+
+
+const onInsert = useCallback(text => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos(todos => todos.concat(todo));
+    nextId.current += 1; // nextId 1씩 더하기
+  }, []);
+
+
+
+const onRemove = useCallback(id => {
+    setTodos(todos => todos.filter(todo => todo.id !== id));
+  }, []);
+
+
+
+const onToggle = useCallback(id => {
+    setTodos(todos =>
+      todos.map(todo =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  }, []);
+
+
+
+return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
+
+
+
+export default App;
