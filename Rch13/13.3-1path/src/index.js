3b570023d114462466c9ec109d698ee34b1fe901
hyejin <reall0108@naver.com>
2022-09-25 16:41:03 +0900

    13.3-1

---
+++ Rch13/13.3-1path/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import * as reportWebVitals from './reportWebVitals';
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+
+
+reportWebVitals();
