3d15376813c973e261efdcaf27b40d12bf416e6b
hyyejin <111478153+hyyejin@users.noreply.github.com>
2022-09-25 18:24:13 +0900

    Update Profiles.js

--- Rch13/13.6.2withrouter/src/Profiles.js
+++ Rch13/13.6.2withrouter/src/Profiles.js
@@ -7,8 +7,22 @@ import WithRouterSample from './WithRouterSample';
 const Profiles = () => {
   return (
     <div>
-      (…)
-      <WithRouterSample />
+      <h3>사용자 목록:</h3>
+      <ul>
+        <li>
+          <Link to="/profiles/velopert">velopert</Link>
+        </li>
+        <li>
+          <Link to="/profiles/gildong">gildong</Link>
+        </li>
+      </ul>
+ 
+      <Route
+        path="/profiles"
+        exact
+        render={() => <div>사용자를 선택해 주세요.</div>}
+      />
+      <Route path="/profiles/:username" component={Profile} />
     </div>
   );
 };
