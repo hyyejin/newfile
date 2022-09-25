ef23b066867cf210540ea5c7e095c122e6ac2f6f
hyejin <reall0108@naver.com>
2022-09-25 18:00:34 +0900

    13.6.2

---
+++ Rch13/13.6.2withrouter/src/Profiles.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import Profile from './Profile';
+import WithRouterSample from './WithRouterSample';
+
+
+const Profiles = () => {
+  return (
+    <div>
+      (…)
+      <WithRouterSample />
+    </div>
+  );
+};
+
+
+
+export default Profiles;
+
