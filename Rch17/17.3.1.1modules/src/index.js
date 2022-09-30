4370f334930b7ec5b82aa73b95e61710ebb12462
hyejin <reall0108@naver.com>
2022-09-30 10:19:27 +0900

    17.6.1.1

--- Rch17/17.3.1.1modules/src/index.js
+++ Rch17/17.3.1.1modules/src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import {createStore} from 'redux';
 import  {Provider} from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import rootReducer from './modules';
-
+const root = createRoot(document.getElementById("root"));
 const store = createStore(rootReducer, composeWithDevTools());
 
-ReactDOM.render(
+root.render(
   <Provider store = {store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
