a53559146803a8d061d32bdbd2b326aa522f9ac1
hyejin <reall0108@naver.com>
2022-09-30 10:54:10 +0900

    17.6.2-1

---
+++ Rch17/17.6.2immer/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Todos from './components/Todos';
+import CounterContainer from './containers/CounterContainer';
+
+
+const App = () => {
+  return (
+    <div>
+      <CounterContainer />
+      <hr />
+      <Todos />
+    </div>
+  );
+};
+
+
+
+export default App;
