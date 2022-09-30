1b895e9a8ce72d788bf6782387a034634526e801
hyejin <reall0108@naver.com>
2022-09-30 11:23:58 +0900

    17.7.6

---
+++ Rch17/17.7.6connect/src/containers/TodosContainer.js
@@ -0,0 +1,32 @@
+import React, from 'react';
+import { useSelector } from 'react-redux';
+import { changeInput, insert, toggle, remove } from '../modules/todos';
+import Todos from '../components/Todos';
+import useAction from '../lib/useActions';
+
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos
+  }));
+  const dispatch = useDispatch();
+  const onChangeInput = useCallback(input => dispatch(changeInput(input)), [
+    dispatch
+  ]);
+  const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
+  const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
+  const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+
+  return (
+    <Todos
+      input={input}
+      todos={todos}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
+    />
+  );
+};
+
+export default React.memo(TodosContainer);
---
+++ Rch17/17.7.6connect/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import {createStore} from 'redux';
+import  {Provider} from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import './index.css';
+import App from './App';
+import rootReducer from './modules';
+const root = createRoot(document.getElementById("root"));
+const store = createStore(rootReducer, composeWithDevTools());
+
+root.render(
+  <Provider store = {store}>
+    <App />
+  </Provider>,
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
