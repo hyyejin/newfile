c3a4debaa2de2b9406ad1a5f8fe2a370ea0a6b16
hyejin <reall0108@naver.com>
2022-10-02 12:47:43 +0900

    18.3.1.5

---
+++ Rch18/18.3.1.5requestthunk/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SampleContainer from './containers/SampleContainer';
+
+const App = () => {
+  return (
+    <div>
+      <SampleContainer />
+    </div>
+  );
+};
+
+export default App;
+
+
+
