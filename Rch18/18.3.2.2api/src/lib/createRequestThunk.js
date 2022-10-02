ab0bae7978b98536458c9921ff3e96badd405007
hyejin <reall0108@naver.com>
2022-10-02 13:42:30 +0900

    18.3.2.3

--- Rch18/18.3.2.2api/src/lib/createRequestThunk.js
+++ Rch18/18.3.2.2api/src/lib/createRequestThunk.js
@@ -1,30 +1,32 @@
+import { call, put } from 'redux-saga/effects';
 import { startLoading, finishLoading } from '../modules/loading';
 
-export default function createRequestThunk(type, request) {
-    // 성공 및 실패 액션 타입을 정의합니다.
-    const SUCCESS = `${type}_SUCCESS`;
-    const FAILURE = `${type}_FAILURE`
-    return params => async dispatch => {
-      dispatch({ type }); // 시작됨
-      dispatch(startLoading(type));
-      try {
-        const response = await request(params);
-        dispatch({
-          type: SUCCESS,
-          payload: response.data
-        }); // 성공
-        dispatch(finishLoading(type));
-      } catch (e) {
-        dispatch({
-          type: FAILURE,
-          payload: e,
-          error: true
-        }); // 에러 발생
-        dispatch(startLoading(type));
-        throw e;
-      }
-    };
-  }
-  
+
+export default function createRequestSaga(type, request) {
+  const SUCCESS = </span><span class="co49">${</span><span class="cd2 co34">type</span><span class="co33">}</span><span class="cd2 co31">_SUCCESS;
+  const FAILURE = </span><span class="co49">${</span><span class="cd2 co34">type</span><span class="co33">}</span><span class="cd2 co31">_FAILURE;
+
+
+
+return function*(action) {
+    yield put(startLoading(type)); // 로딩 시작
+    try {
+      const response = yield call(request, action.payload);
+      yield put({
+        type: SUCCESS,
+        payload: response.data
+      });
+    } catch (e) {
+      yield put({
+        type: FAILURE,
+        payload: e,
+        error: true
+      });
+    }
+    yield put(finishLoading(type)); // 로딩 끝
+  };
+}
+
+
   
   // 사용법: createRequestThunk(‘GET_USERS‘,api.getUsers);
