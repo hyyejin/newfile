3bc9c4a25dc07c18aa32ae681b6a5d44404cf880
hyejin <reall0108@naver.com>
2022-10-06 16:35:25 +0900

    23.2,23.2.1

---
+++ Rch23/user/src/models/user.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from 'mongoose';
+
+
+const UserSchema = new Schema({
+  username: String,
+  hashedPassword: String,
+});
+
+
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
