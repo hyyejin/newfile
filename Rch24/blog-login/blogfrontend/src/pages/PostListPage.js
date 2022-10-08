f176fa84a95ea9eb4d8ed1ab759f4173444093cb
hyejin <reall0108@naver.com>
2022-10-08 14:27:39 +0900

    24.2.4-2, 24.2.5, 24.2.6, 24.3.1-1

---
+++ Rch24/blog-login/blogfrontend/src/pages/PostListPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/common/Header';
+
+const PostListPage = () => {
+  return (
+    <>
+    <Header />
+    <div>안녕하세요.</div>
+  </>
+  );
+};
+
+
+
+export default PostListPage;
